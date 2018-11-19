77efcad3b838d8f7cabe49b816b8686dacbe23f9
Kihwan.Kim <superhuman54@gmail.com>
2018-11-20 01:17:47 +0900

    add click to login button

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('http://www.google.com/ncr');
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
+    await driver.get('https://www.nike.com/kr/ko_kr');
+      await driver.findElement(By.className('login')).click();
+      const textfield = await driver.findElement(By.className("uk-form-row"));
+      await textfield.findElement(By.name('j_username')).sendKeys("superhuman54", Key.TAB)
+    // await driver.findElement(By.css('j_username')).sendKeys('superhuman54', Key.TAB).then()
+    // await driver.wait(until.titleIs('webdriver - Google Search'), 1000);
   } finally {
-    await driver.quit();
+
   }
 })();
 
